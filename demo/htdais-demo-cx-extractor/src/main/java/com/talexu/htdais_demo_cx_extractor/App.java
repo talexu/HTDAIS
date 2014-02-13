c8adc3ebb44ad9c1ac37192d60c9eafa73ce4d3c
talexu <bjutales@hotmail.com>
2014-02-13 18:30:02 +0800

    添加从html提取正文的Demo

---
+++ demo/htdais-demo-cx-extractor/src/main/java/com/talexu/htdais_demo_cx_extractor/App.java
@@ -0,0 +1,54 @@
+package com.talexu.htdais_demo_cx_extractor;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) throws ClientProtocolException, IOException {
+		System.out.println("Hello World!");
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("http://news.qq.com/a/20140212/006485.htm");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            System.out.println("----------------------------------------");
+            String html = new TextExtract().parse(responseBody);
+            System.out.println(html);
+            
+        } finally {
+            httpclient.close();
+        }
+	}
+}
