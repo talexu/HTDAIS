d35050535ac1f13eb2a418db9d5405a5b4f26265
talexu <bjutales@hotmail.com>
2014-02-17 01:13:57 +0800

    修改WithDBSCAN的distance方法为protected

--- src/bjutales-moa-htdais/moa/src/main/java/moa/clusterers/denstream/WithDBSCAN.java
+++ src/bjutales-moa-htdais/moa/src/main/java/moa/clusterers/denstream/WithDBSCAN.java
@@ -280,7 +280,7 @@ public class WithDBSCAN extends AbstractClusterer {
 
 	}
 
-	private double distance(double[] pointA, double[] pointB) {
+	protected double distance(double[] pointA, double[] pointB) {
 		double distance = 0.0;
 		for (int i = 0; i < pointA.length; i++) {
 			double d = pointA[i] - pointB[i];
