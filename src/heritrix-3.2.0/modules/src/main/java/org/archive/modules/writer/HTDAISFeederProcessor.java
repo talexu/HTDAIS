c0bac0a08a1252260d7f3f82042cefd1f9b146a0
talexu <bjutales@hotmail.com>
2014-02-20 21:53:59 +0800

    test

--- src/heritrix-3.2.0/modules/src/main/java/org/archive/modules/writer/HTDAISFeederProcessor.java
+++ src/heritrix-3.2.0/modules/src/main/java/org/archive/modules/writer/HTDAISFeederProcessor.java
@@ -57,41 +57,41 @@ public class HTDAISFeederProcessor extends Processor {
 		// IOUtils.closeQuietly(fos);
 		// }
 
-		File outputFile = new File("/Users/bjutales/Downloads/TestCrawl/"
-				+ count++ + ".txt");
-		RecordingInputStream recis = uri.getRecorder().getRecordedInput();
-		if (0L == recis.getResponseContentLength()) {
-			return;
-		}
-		try {
-			writeToPath(recis, outputFile);
-
-		} catch (IOException ioException) {
-			System.err.println("File Error!");
-		} finally {
-			System.out.println("Mission complete!");
-			flag = false;
-		}
+//		File outputFile = new File("/Users/bjutales/Downloads/TestCrawl/"
+//				+ count++ + ".txt");
+//		RecordingInputStream recis = uri.getRecorder().getRecordedInput();
+//		if (0L == recis.getResponseContentLength()) {
+//			return;
+//		}
+//		try {
+//			writeToPath(recis, outputFile);
+//
+//		} catch (IOException ioException) {
+//			System.err.println("File Error!");
+//		} finally {
+//			System.out.println("Mission complete!");
+//			flag = false;
+//		}
 	}
 
-	private void writeToPath(RecordingInputStream recis, File dest)
-			throws IOException {
-		File tf = new File(dest.getPath() + "N");
-		ReplayInputStream replayis = null;
-		FileOutputStream fos = null;
-		try {
-			replayis = recis.getMessageBodyReplayInputStream();
-			fos = new FileOutputStream(tf);
-
-			replayis.readFullyTo(fos);
-		} finally {
-			IOUtils.closeQuietly(replayis);
-			IOUtils.closeQuietly(fos);
-		}
-		if (!tf.renameTo(dest)) {
-			throw new IOException("Can not rename " + tf.getAbsolutePath()
-					+ " to " + dest.getAbsolutePath());
-		}
-
-	}
+	// private void writeToPath(RecordingInputStream recis, File dest)
+	// throws IOException {
+	// File tf = new File(dest.getPath() + "N");
+	// ReplayInputStream replayis = null;
+	// FileOutputStream fos = null;
+	// try {
+	// replayis = recis.getMessageBodyReplayInputStream();
+	// fos = new FileOutputStream(tf);
+	//
+	// replayis.readFullyTo(fos);
+	// } finally {
+	// IOUtils.closeQuietly(replayis);
+	// IOUtils.closeQuietly(fos);
+	// }
+	// if (!tf.renameTo(dest)) {
+	// throw new IOException("Can not rename " + tf.getAbsolutePath()
+	// + " to " + dest.getAbsolutePath());
+	// }
+	//
+	// }
 }
