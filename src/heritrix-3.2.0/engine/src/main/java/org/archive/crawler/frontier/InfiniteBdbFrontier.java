00a3e4ef281c1dc06c56bc5e791f8cc649f019fe
talexu <bjutales@hotmail.com>
2014-02-24 22:46:47 +0800

    优化重复抓取流程，增加延迟选项

--- src/heritrix-3.2.0/engine/src/main/java/org/archive/crawler/frontier/InfiniteBdbFrontier.java
+++ src/heritrix-3.2.0/engine/src/main/java/org/archive/crawler/frontier/InfiniteBdbFrontier.java
@@ -1,5 +1,10 @@
 package org.archive.crawler.frontier;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.archive.modules.CrawlURI;
 
 public class InfiniteBdbFrontier extends BdbFrontier {
@@ -16,17 +21,37 @@ public class InfiniteBdbFrontier extends BdbFrontier {
 		super.addedSeed(puri);
 	}
 
+	ScheduledExecutorService scheduledThreadPool = Executors
+			.newScheduledThreadPool(5);
+	AtomicInteger emptyQuery = new AtomicInteger(0);
+
+	/** Delay before recrawl */
+	protected long delay = 500;
+
+	public long getDelay() {
+		return delay;
+	}
+
+	public void setDelay(long delay) {
+		this.delay = delay;
+	}
+
 	/**
 	 * If frontier is exhausted, reload seeds
 	 * 
 	 * @see org.archive.crawler.framework.Frontier#isEmpty()
 	 */
 	public boolean isEmpty() {
-		if (queuedUriCount.get() == 0
-				&& (uriUniqFilter == null || uriUniqFilter.pending() == 0)
-				&& futureUriCount.get() == 0) {
-			getSeeds().announceSeeds();
+		boolean empty = super.isEmpty();
+		if (empty && emptyQuery.getAndAdd(1) == 0) {
+			scheduledThreadPool.schedule(new Runnable() {
+				@Override
+				public void run() {
+					getSeeds().announceSeeds();
+					emptyQuery.set(0);
+				}
+			}, getDelay(), TimeUnit.SECONDS);
 		}
-		return false;
+		return empty;
 	}
 }
